4c1df2e4a73ec5a598624fa63d410722b22ffb16
Shoko <shoko.hs89@gmail.com>
2025-02-09 17:33:59 +0900

    Create 2_A.py

---
+++ Lesson/ITP1/2_A.py
@@ -0,0 +1,16 @@
+# 大小関係
+
+a, b = map(int, input().split())
+
+if a < b:
+    print("a < b")
+elif a > b:
+    print("a > b")
+else:
+    print("a == b")
+
+# 解いたChallenge
+# PCK Prelim 0358
+
+# 未回答のChallenge
+# JOI Prelim 0630
