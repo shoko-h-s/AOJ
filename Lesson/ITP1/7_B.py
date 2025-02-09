880b07f3883009c3bc91625f8b0447faaa8ebdaa
Shoko <shoko.hs89@gmail.com>
2025-02-09 23:02:34 +0900

    Update 7_B.py

--- Lesson/ITP1/7_B.py
+++ Lesson/ITP1/7_B.py
@@ -11,18 +11,18 @@ while True:
     if (n == 0) and (x == 0):
         break
         
-    else:
-        # 1～nまでの数値のリストを作る
-        numbers = [i+1 for i in range(n)]
-        
-        # 組み合わせを列挙する
-        for a in itertools.combinations(numbers, 3):
-            if sum(a) == x:
-                counter += 1
-                
-        results.append(counter)
+    # 1～nまでの数値のリストを作る
+    numbers = [i+1 for i in range(n)]
+    
+    # 組み合わせを列挙する
+    for a in itertools.combinations(numbers, 3):
+        if sum(a) == x:
+            counter += 1
+            
+    results.append(counter)
         
 for r in results:
     print(r)
-
+    
+# 解いたChallenge
 # ICPC Prelim 1608
