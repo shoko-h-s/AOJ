d9ed559d152fb91009d74f7d2737226f751fb853
Shoko <shoko.hs89@gmail.com>
2025-02-09 21:49:29 +0900

    Create 7_B.py

---
+++ Lesson/ITP1/7_B.py
@@ -0,0 +1,28 @@
+# 組み合わせの数
+
+import itertools
+
+results = []
+
+while True:
+    counter = 0    
+    n, x = map(int, input().split())
+
+    if (n == 0) and (x == 0):
+        break
+        
+    else:
+        # 1～nまでの数値のリストを作る
+        numbers = [i+1 for i in range(n)]
+        
+        # 組み合わせを列挙する
+        for a in itertools.combinations(numbers, 3):
+            if sum(a) == x:
+                counter += 1
+                
+        results.append(counter)
+        
+for r in results:
+    print(r)
+
+# ICPC Prelim 1608
