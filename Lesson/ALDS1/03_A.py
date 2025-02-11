aaa97b9a12f9976b8a106f4c4031f148ab2fe66d
Shoko <shoko.hs89@gmail.com>
2025-02-11 22:56:25 +0900

    Create 03_A.py

---
+++ Lesson/ALDS1/03_A.py
@@ -0,0 +1,53 @@
+# スタック
+# 逆ポーランド記法
+
+stack = []
+
+# 要素の追加
+def s_push(x):
+    stack.append(x)
+
+# 要素を取り出す関数（逆ポーランド記法ではあまり使わない）
+def s_pop():
+    return stack.pop()
+
+def s_plus():
+    # 別途、アキュムレータ（計算機）用の変数を準備
+    acc = stack[-1]
+    stack.pop()
+    acc += stack[-1]
+    stack.pop()
+    stack.append(acc)
+    
+def s_minus():
+    acc = stack[-1]
+    stack.pop()
+    acc = stack[-1] - acc
+    stack.pop()
+    stack.append(acc)
+    
+def s_multi():
+    acc = stack[-1]
+    stack.pop()
+    acc *= stack[-1]
+    stack.pop()
+    stack.append(acc)
+
+a_list = input().split()
+
+for a in a_list:
+    
+    # 数値である場合
+    if a.isdigit():
+        s_push(int(a))
+        
+    elif a == "+":
+        s_plus()
+        
+    elif a == "-":
+        s_minus()
+        
+    elif a == "*":
+        s_multi()
+        
+print(stack[0])
